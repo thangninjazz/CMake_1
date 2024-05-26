05b59beddcdfaa8e7d90c6909273f947e5c02cb0
Do Van Thang <thang.dovan@samsung.com>
2024-05-26 16:34:16 +0700

    Sua file hello

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,6 +1,13 @@
 #include<iostream>
+using namespace std;
 
 int main(int argc, char *argv[]){
-   std::cout << "Hello World!" << std::endl;
+    int number;
+    cout << "Nhap so tu nhien: ";
+    cin >> number;
+    
+   cout << "Hello World!" << endl;
+   cout << "Day la so: " << number << endl;
+   system("pause");
    return 0;
 }
